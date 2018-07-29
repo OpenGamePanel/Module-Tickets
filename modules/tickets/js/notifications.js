4ff607fd638dcc065dd34a0e08a0e35f8f0a1b7d
®omano <omano@redorchestra2.fr>
2018-07-29 03:34:08 +0200

    little modification

--- modules/tickets/js/notifications.js
+++ modules/tickets/js/notifications.js
@@ -2,7 +2,7 @@ $(function() {
 	$.getJSON("home.php?m=tickets&p=notifications&type=cleared", function(data) {
 		if (data['notificationCount'] > 0) {
 			var menutickets = document.getElementsByClassName('tickets');
-			menutickets[0].innerHTML += "&nbsp; <text style='background:#aa0000;color:white;border-radius:20px;border:1px solid white;'>&nbsp;&nbsp;<b>" + data['notificationCount'] + "</b>&nbsp;&nbsp;</text>";
+			menutickets[0].innerHTML += "<text style='background:#aa0000;color:white;border-radius:20px;border:1px solid white;margin-left:5px'>&nbsp;&nbsp;<b>" + data['notificationCount'] + "</b>&nbsp;&nbsp;</text>";
 		}
 	});
 });
